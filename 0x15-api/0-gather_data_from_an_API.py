8e44162def5257dea6b1ba3ea1014f7f65625919
chrisokwisa <okwisachristabel@gmail.com>
2022-10-21 08:08:43 +0000

    Gathers information from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""module that returns information about TODO list"""
+"""gets api"""
 import requests
 from sys import argv
 
-def todo(employeeID):
-    """displays the employee TODO list in progress"""
+
+def todo(userid):
+    """Gets all information about employees progress in tasks done"""
     employee_name = requests.get(
-            'https://jsonplaceholder.typicode.com/users/{}'.format(
-                employeeID)).json().get('employee')
-    tasks = requests.get(
-            'https://jsonplaceholder.typicode.com/{}/todos'.format(
-                employeeID)).json()
-    tasks_done = ['\t {}\n'.format(dic.get('title')) for dic in tasks
-            if dic.get(completed)]
-    if employee_name and tasks:
-        print("Employee {} is done with tasks({}/{}:".format
-            (employee_name, len(tasks_done), len(tasks)))
-        print(''.join(task_done), end='')
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            userid)).json().get('name')
+    number_of_tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            userid)).json()
+    tasks_done = ['\t {}\n'.format(dic.get('title')) for dic in number_of_tasks
+                 if dic.get('completed')]
+    if employee_name and number_of_tasks:
+        print("Employee {} is done with tasks({}/{}):".format
+              (employee_name, len(tasks_done), len(number_of_tasks)))
+        print(''.join(tasks_done), end='')
 
 
 if __name__ == "__main__":
