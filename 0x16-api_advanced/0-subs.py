a97298f13113016b3ba201c0dfe6465f48492d09
chrisokwisa <okwisachristabel@gmail.com>
2022-11-02 07:53:19 +0000

    api advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Returns the number of subscribers """
+from sys import argv
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """ subscriptions of the channel """
+    head = {'User-Agent': 'Abracadabra'}
+    count = get('https://www.reddit.com/r/{}/about/json'.format(
+        subreddit), headers=head).json()
+    try:
+        return count.get('data').get('subscribers')
+    except:
+        return 0
+
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
