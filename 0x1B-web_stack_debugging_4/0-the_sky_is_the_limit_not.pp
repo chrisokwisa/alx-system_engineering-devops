9649aac77df393cd2af2bf2bf714fa1e03f73e04
chrisokwisa <okwisachristabel@gmail.com>
2022-11-10 11:34:09 +0000

    How to fix failed requests when using apacheBench

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # fixes ApacheBench to simulate HTTP requests to a web server
 exec {'fixes failed requests':
-    command => 'sed -i "s/15/2000/g" /etc/nginx/default',
-    path    => '/bin/:sbin/:/usr/bin/:/usr/sbin/',
-    onlyif  => 'test -f /etc/default/nginx'
+   command => 'sed -i "s/15/2000/g" /etc/nginx/default',
+   path    => '/bin/:sbin/:/usr/bin/:/usr/sbin/',
+   onlyif  => 'test -f /etc/default/nginx'
 }
